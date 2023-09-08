a23bab2472f66172f4c226cd353b4b5563a55e05
Maykon Sousa <maykon.sousa@hotmail.com>
2023-09-08 10:17:09 -0300

    feat: uplgrade colabPage

--- src/screens/Colabs/Colabs.styles.ts
+++ src/screens/Colabs/Colabs.styles.ts
@@ -1,8 +1,7 @@
-import styled from "styled-components/native";
+import styled, {css} from "styled-components/native";
 
 export const ColabsContainer = styled.View`
     flex: 1;
-    align-items: center;
     background-color: ${({theme}) =>theme.colors.gray_600};
     padding: 16px 24px;
     padding-top: 40px;
@@ -12,4 +11,22 @@ export const FormContainer = styled.View`
     width: 100%;
     flex-direction: row;
     align-items: center;
+    background-color: ${({theme}) =>theme.colors.gray_700};
+    border-radius: 6px;
+    margin-bottom: 16px;
+`;
+
+export const TabContainer = styled.View`
+width: 100%;
+flex-direction: row;
+align-items: center;
+`;
+
+export const  CounterPlayers = styled.Text`
+${({theme}) => css`
+    font-family: ${theme.fonts.bold};
+    font-size: ${theme.font_size.sm}px;
+    color: ${theme.colors.gray_300};
+    margin-right: 4px;
+`}
 `;
--- src/screens/Colabs/Colabs.tsx
+++ src/screens/Colabs/Colabs.tsx
@@ -1,11 +1,48 @@
-import React from 'react'
-import { ColabsContainer, FormContainer } from './Colabs.styles'
+import {FlatList} from 'react-native';
+import React, {useState} from 'react';
+import { ColabsContainer, CounterPlayers, FormContainer, TabContainer } from './Colabs.styles'
 import { Header } from '@components/Header'
 import { PageTitle } from '@components/PageTitle'
 import { Input } from '@components/Input'
 import { HandleIcon } from '@components/HandleIcon'
+import { TabTeam } from '@components/TabTeam'
+import uuid from 'react-native-uuid';
+import { PlayerCard } from '@components/PlayerCard';
+
+export interface IPlayer {
+  name: string
+  team: string
+  subTeam: string
+  id: string
+}
+
 
 export const Colabs = () => {
+  const [subTeams, setSubTeams] = useState(['time a', 'time b'])
+  const [teamSelected, setTeamSelected] = useState('time a')
+  const [players, setPlayers] = useState<IPlayer[]>([])
+  const [playerName, setPlayerName] = useState('')
+
+  const filteredPlayers = players.filter((player) => player.subTeam === teamSelected)
+ 
+
+  const handleAddPlayer = () => {
+    if(playerName) {
+      const newPlayer: IPlayer = {
+        name: playerName,
+        team: 'Teste',
+        subTeam: teamSelected,
+        id: uuid.v4() as string
+      }
+      setPlayers([...players, newPlayer])
+      setPlayerName('')
+    }
+  }
+
+  const handleRemovePlayer = (id: string) => {
+    const filteredPlayers = players.filter((player) => player.id !== id)
+    setPlayers(filteredPlayers)
+  }
   return (
     <ColabsContainer>
     <Header showBackButton />
@@ -15,9 +52,53 @@ export const Colabs = () => {
      />
 
      <FormContainer>
-      <Input placeholder="Nome" />
-      <HandleIcon type="add" />
+      <Input 
+      placeholder="Nome"
+      value={playerName}
+      onChangeText={(text) => setPlayerName(text)}
+       />
+      <HandleIcon type="add" onPress={handleAddPlayer} />
      </FormContainer>
+
+     <TabContainer>
+     <FlatList 
+      horizontal
+      data={subTeams}
+      keyExtractor={(item) => item}
+      renderItem={({item}) => (
+        <TabTeam 
+        label={item}
+        isActive={item === teamSelected}
+        onPress={() => setTeamSelected(item)}
+        />
+        
+      )}
+     
+     />
+     <CounterPlayers>
+      {filteredPlayers.length} 
+     </CounterPlayers>
+     </TabContainer>
+     <FlatList 
+      data={filteredPlayers}
+      keyExtractor={(item) => item.id}
+      renderItem={({item}) => (
+        <PlayerCard player={item} onRemovePlayer={handleRemovePlayer} />
+      )}
+      ListEmptyComponent={() => (
+        <PageTitle
+        title={`Nenhum jogador no time ${teamSelected.toUpperCase()}`}
+        subtitle="Adicione jogadores para começar"
+        />
+      )}
+      contentContainerStyle={
+        [
+          {paddingBottom: 100},
+          filteredPlayers.length === 0 && {flex: 1, justifyContent: 'center'}
+        ]
+      }
+      showsVerticalScrollIndicator={false}
+     />
 
      
     </ColabsContainer>
